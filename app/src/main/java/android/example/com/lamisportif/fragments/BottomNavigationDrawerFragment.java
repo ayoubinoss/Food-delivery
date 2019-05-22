60d54b6999c23e5384feff1ea01f5df63a23a858
ayoubinoss <ayoubaitbahaddou@gmail.com>
2019-05-22 21:12:23 +0000

    cart layout created

--- app/src/main/java/android/example/com/lamisportif/fragments/BottomNavigationDrawerFragment.java
+++ app/src/main/java/android/example/com/lamisportif/fragments/BottomNavigationDrawerFragment.java
@@ -1,9 +1,14 @@
 package android.example.com.lamisportif.fragments;
 
+import android.app.Dialog;
 import android.example.com.lamisportif.R;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,18 +17,62 @@ import android.widget.Toast;
 
 public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
 
-    @Override
+    private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss();
+            }
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+        }
+    };
+    /*@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.
                 inflate(R.layout.fragment_bottom_navigation_drawer_cart, container, false);
         return view;
-    }
+    }*/
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ImageView closeButton = getView().findViewById(R.id.close_imageview);
+
+
+    }
+
+    @Override
+    public void setupDialog(Dialog dialog, int style) {
+        super.setupDialog(dialog, style);
+        View inflatedView = View.inflate(getContext(), R.layout.fragment_bottom_navigation_drawer_cart, null);
+        dialog.setContentView(inflatedView);
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) ((View) inflatedView.getParent()).getLayoutParams();
+        CoordinatorLayout.Behavior behavior = params.getBehavior();
+
+        if (behavior != null && behavior instanceof BottomSheetBehavior) {
+            ((BottomSheetBehavior) behavior).setBottomSheetCallback(mBottomSheetBehaviorCallback);
+        }
+
+        View parent = (View) inflatedView.getParent();
+        parent.setFitsSystemWindows(true);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(parent);
+        inflatedView.measure(0, 0);
+        DisplayMetrics displaymetrics = new DisplayMetrics();        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int screenHeight = displaymetrics.heightPixels;
+        bottomSheetBehavior.setPeekHeight(screenHeight);
+
+        if (params.getBehavior() instanceof BottomSheetBehavior) {
+            ((BottomSheetBehavior)params.getBehavior()).setBottomSheetCallback(mBottomSheetBehaviorCallback);
+        }
+
+        params.height = screenHeight;
+        parent.setLayoutParams(params);
+
+        ImageView closeButton = inflatedView.findViewById(R.id.close_imageview);
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +80,5 @@ public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
                 dismiss();
             }
         });
-
     }
 }
