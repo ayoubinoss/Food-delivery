b15dfd837d89f9f865c7db6157239e1b16a0d84c
ayoubinoss <ayoubaitbahaddou@gmail.com>
2019-05-23 01:41:18 +0000

    adding search icon and some stuff

--- app/src/main/java/android/example/com/lamisportif/MainActivity.java
+++ app/src/main/java/android/example/com/lamisportif/MainActivity.java
@@ -1,15 +1,20 @@
 package android.example.com.lamisportif;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.example.com.lamisportif.fragments.BottomNavigationDrawerFragment;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -32,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
                 switch(menuItem.getItemId()) {
-                    case R.id.search:
-                        Toast.makeText(getApplication(),"my Search", Toast.LENGTH_SHORT).show();
+                    case R.id.filter:
+                        Toast.makeText(getApplication(),"my Filter", Toast.LENGTH_SHORT).show();
                         break;
                 }
                 return true;
@@ -55,12 +60,33 @@ public class MainActivity extends AppCompatActivity {
                 f.show(getSupportFragmentManager(),"TAG");
             }
         });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.home_top_menu,menu);
+
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(getApplication(), "hello " + query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(TextUtils.isEmpty(newText)) {
+                    Toast.makeText(getApplication(),"empty",Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
         return true;
     }
 
@@ -68,10 +94,24 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch(item.getItemId()) {
-            case R.id.logout:
-                Toast.makeText(getApplication(),"deconnexion..", Toast.LENGTH_SHORT).show();
+            case R.id.search:
+                Toast.makeText(getApplication(),"search..", Toast.LENGTH_SHORT).show();
         }
 
         return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this,"you type : " + query, Toast.LENGTH_SHORT).show();
+        }
     }
 }
