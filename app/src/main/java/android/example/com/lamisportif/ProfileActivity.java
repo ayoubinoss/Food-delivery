49e5630ad9650879af188df9d196985f2173c307
ayoubinoss <ayoubaitbahaddou@gmail.com>
2019-05-24 15:39:50 +0000

    some items added

--- app/src/main/java/android/example/com/lamisportif/ProfileActivity.java
+++ app/src/main/java/android/example/com/lamisportif/ProfileActivity.java
@@ -1,13 +1,44 @@
 package android.example.com.lamisportif;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.edit:
+                Toast.makeText(this,"edit",Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                this.finish();
+                break;
+        }
+        return true;
     }
 }
