29d735db2790007789a5087945baa508d7dd10f8
ayoubinoss <ayoubaitbahaddou@gmail.com>
2019-05-28 05:27:01 +0000

    profile activity modifications

--- app/src/main/java/android/example/com/lamisportif/FilterActivity.java
+++ app/src/main/java/android/example/com/lamisportif/FilterActivity.java
@@ -1,7 +1,9 @@
 package android.example.com.lamisportif;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class FilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+        }
+        return true;
     }
 }
--- app/src/main/java/android/example/com/lamisportif/fragments/RegisterFragment.java
+++ app/src/main/java/android/example/com/lamisportif/fragments/RegisterFragment.java
@@ -21,10 +21,19 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 public class RegisterFragment extends Fragment {
@@ -32,6 +41,9 @@ public class RegisterFragment extends Fragment {
     private TextInputEditText mEmail;
     private TextInputEditText mPassword;
     private TextInputEditText mRepeatPassword;
+    private TextInputEditText mPhoneNumber;
+    private TextInputEditText mName;
+
     private MaterialButton mButtonCancel;
     private MaterialButton mButtonRegister;
     private MaterialButton mButtonSignIn;
@@ -49,12 +61,18 @@ public class RegisterFragment extends Fragment {
 
         //initialize FirebaseAuth object
         mAuth = FirebaseAuth.getInstance();
+
         //initialize progressDialog object
         progressDialog = new ProgressDialog(getActivity());
+
         //initialize views
         mEmail = view.findViewById(R.id.email_edit_text);
         mPassword = view.findViewById(R.id.password_edit_text);
         mRepeatPassword = view.findViewById(R.id.r_password_edit_text);
+        mPhoneNumber = view.findViewById(R.id.phone_number);
+        mName = view.findViewById(R.id.name_edit_text);
+
+        //initialize buttons
         mButtonCancel = view.findViewById(R.id.cancel_btn);
         mButtonRegister = view.findViewById(R.id.register_btn);
         mButtonSignIn = view.findViewById(R.id.sign_in_btn);
@@ -78,16 +96,28 @@ public class RegisterFragment extends Fragment {
                 if(!isEmailValid(mEmail.getText())) {
                     mEmail.setError(getString(R.string.error_msg_email));
                 }
-                //fix this
+                //check password
                 if(!isRepeatPasswordValid(String.valueOf(mPassword.getText()),String.valueOf(mRepeatPassword.getText()))){
                     mRepeatPassword.setError(getString(R.string.error_msg_r_password));
+                }
+                //check phone number
+                if(!isPhoneNumberValid(mPhoneNumber.getText())) {
+                    mPhoneNumber.setError(getString(R.string.error_msg_phone));
+                }
+                //check name
+                if(!isNameValid(mName.getText())) {
+                    mName.setError(getString(R.string.name_msg_error));
                 }
                 else{
                     progressDialog.setMessage("Registering User ...");
                     progressDialog.show();
+
                     mEmail.setError(null); //clear Error
                     mPassword.setError(null); //clear Error
                     mRepeatPassword.setError(null); // clear Error
+                    mName.setError(null);
+                    mPhoneNumber.setError(null);
+
                     mAuth.createUserWithEmailAndPassword(
                             String.valueOf(mEmail.getText()),
                             String.valueOf(mPassword.getText()))
@@ -137,17 +167,13 @@ public class RegisterFragment extends Fragment {
      */
     private void updateUI(FirebaseUser currentUser) {
         if (currentUser != null) {
-            //the user is already authenticated we display the catalog
-            FirebaseAuth.getInstance().signOut();
-            ((NavigationHost)getActivity()).navigateTo(new LoginFragment(), true);
-            Toast.makeText(getContext(), "An email was sent to your newslettre check your " +
-                    "email letter to activate your account", Toast.LENGTH_LONG).show();
+            storeUserInformations();
         }
     }
 
     /**
      * check if email is valid eg: abcd@cdefh.xyz
-     * @param text
+     * @param text email to verify
      * @return boolean
      */
     public boolean isEmailValid(@Nullable Editable text) {
@@ -156,7 +182,7 @@ public class RegisterFragment extends Fragment {
     }
     /**
      * check if the password is not empty
-     * @param text
+     * @param text password to verify
      * @return boolean
      */
     private boolean isPasswordValid(Editable text) {
@@ -164,12 +190,67 @@ public class RegisterFragment extends Fragment {
     }
 
     /**
-     * check if the passwords match
-     * @param text2
+     * check if the user name is not empty
+     * @param text name to verify
+     * @return boolean
+     */
+
+    private boolean isNameValid(Editable text) { return !TextUtils.isEmpty(text); }
+
+    /**
+     * check if the phone number is valid
+     * @param text phone to verify
+     * @return boolean
+     */
+    private boolean isPhoneNumberValid(Editable text) { return !TextUtils.isEmpty(text) && text.length()==10;}
+
+    /**
+     * check if password match
+     * @param text the password
+     * @param text2 the password repeated
      * @return boolean
      */
     private boolean isRepeatPasswordValid(String text, String text2) {
         return text.equals(text2);
+    }
+
+    /**
+     * store user details into FireStore
+     */
+    public void storeUserInformations() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> newUser = new HashMap<>();
+        if(mName.getText() != null && mPhoneNumber.getText() != null && mEmail.getText() != null) {
+
+            newUser.put("name", mName.getText().toString());
+            newUser.put("phone_number", mPhoneNumber.getText().toString());
+            newUser.put("email", mEmail.getText().toString());
+        }
+
+        db.collection("users").document(mEmail.getText().toString())
+                .set(newUser)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                        progressDialog.dismiss();
+                        FirebaseAuth.getInstance().signOut();
+                        ((NavigationHost)getActivity()).navigateTo(new LoginFragment(), true);
+                        Toast.makeText(getContext(), "An email was sent to your newslettre check your " +
+                                "email letter to activate your account", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                        progressDialog.dismiss();
+                        FirebaseAuth.getInstance().signOut();
+                        Toast.makeText(getContext(), "try again", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
     }
 
 }
